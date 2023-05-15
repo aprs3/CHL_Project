413c5237e5c51a8305838eb7576ce88da680dfaf
Asduffo <Asduffo@users.noreply.github.com>
2023-05-15 20:32:35 +0200

    Quanto ne so

---
+++ separate_state_markers.R
@@ -0,0 +1,61 @@
+setwd("~/Scrivania/CHL_Project")
+source("utils.R")
+
+dataset = "TI_IMM"
+N <- list("N109389", "N119540", "N130064", "N158891")
+I <- list("I104689", "I130064", "I182231", "I139892")
+H <- list("H101694", "H152638", "H158108", "H180844")
+
+#to_load <- list(N, I, H)
+states_to_load <- list(N, I, H)
+
+
+p <- paste0(getwd(), "/", dataset)
+
+optimal_clusters <- list()
+
+i <- 1
+cells_data  <- read_csv_data(paste0(p, "/known_cells_genes.csv"))
+cells_names <- names(cells_data)
+
+csvs <- list()
+
+#TI_IMM_state_markers_I
+for(i in 1:length(states_to_load))
+{
+  patient_type <- substr(states_to_load[[i]][[1]],1,1)
+  
+  #loads one of the known state markers file from the folder
+  filename <- paste0(p, "/",dataset, "_state_markers_",patient_type, ".csv")
+  curr_csv <- read_csv_data(filename)
+  
+  to_add <- c('cell_name,new_genes,known_genes')
+  
+  #for each cell we now need to get the newly found genes (meaning, the ones not
+  #included in the known_cells_genes.csv file, which can be found by intersection)
+  #they can be obtained simply by subtracting the known_cells_genes from it 
+  for(i in 1:length(cells_data))
+  {
+    #loads the current cell's known genes
+    known_cell_markers <- cells_data[[i]]
+    #print(known_cell_markers)
+    
+    #current genes
+    file_cell_marker <- curr_csv[[i]]
+    
+    #finds the new_genes by set subtraction, and the remainder are the already known genes
+    new_genes           <- paste0(setdiff(file_cell_marker, known_cell_markers), collapse=';')
+    already_known_genes <- paste0(intersect(file_cell_marker, known_cell_markers), collapse=';')
+    
+    tmp <- paste0(list(cells_names[[i]], new_genes, already_known_genes), collapse=',')
+    to_add <- c(to_add, tmp)
+  }
+  
+  path <- paste0(p, "/",dataset, "_state_markers_splitted_", patient_type, ".csv")
+  file.create(path)
+  file_conn = file(path)
+  writeLines(to_add, file_conn)
+  close(file_conn)
+}
+
+
